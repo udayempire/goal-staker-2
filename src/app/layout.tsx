af98b0686f7b9758a76c1f21ed91fc9935957c9f
www.udayempire <147295044+udayempire@users.noreply.github.com>
2025-04-03 00:28:31 +0530

    changed website name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,9 @@ import { ThirdwebProvider } from "thirdweb/react";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "thirdweb SDK + Next starter",
+  title: "Goal Staker",
   description:
-    "Starter template for using thirdweb SDK with Next.js App router",
+    "Go Consistend and win some rewards for it",
 };
 
 export default function RootLayout({
